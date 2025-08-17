c2a84049917fd418d91fb1b4d32c496c64d97e0f
wuhonglei <1017368065@qq.com>
2025-08-17 23:04:41 +0800

    优化用户输入功能，更新学历和薪资选择逻辑，增强数据读取与保存机制，提升代码可读性与结构。

--- src/util/fs.py
+++ src/util/fs.py
@@ -2,15 +2,15 @@ import os
 import json
 
 
-def write_json(data, file_path):
+def write_json(data, file_path: str):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-def read_json(file_path):
+def read_json(file_path: str, default_value: dict | list | None = None):
     if not exists_file(file_path):
-        return []
+        return default_value
 
     with open(file_path, 'r', encoding='utf-8') as f:
         return json.load(f)
--- src/util/input.py
+++ src/util/input.py
@@ -2,31 +2,38 @@ import questionary
 
 from config import degree_map, salary_map
 from local_type import UserInput
+from util.fs import read_json, write_json
 
 
 def collect_user_input(exist_job_details: bool):
+    last_user_input: UserInput = read_json(
+        'data/user_input.json', {})  # type:ignore
+
     # 学历
     degree = questionary.select(
         "你的最高学历?",
+        default=last_user_input.get('degree'),
         choices=list(degree_map.keys())[:3],
     ).ask()
 
     # 薪资
     salary = questionary.select(
         "你的期望薪资?",
+        default=last_user_input.get(
+            'salary') if last_user_input.get('salary') in salary_map else "其他",
         choices=salary_map + ['其他'],
     ).ask()
 
     if salary == '其他':
         salary = questionary.text(
             "请输入具体薪资范围（如：40-50K）:",
-            default="40-50K"
+            default=last_user_input.get('salary', "40-50K")
         ).ask()
 
     # 工作经验
     experience = questionary.text(
         "你的工作经验(如：3年、5年、10年)?",
-        default="3"
+        default=last_user_input.get('experience', "3")
     ).ask()
 
     if exist_job_details:
@@ -37,4 +44,7 @@ def collect_user_input(exist_job_details: bool):
     else:
         user_job_details = False
 
-    return UserInput(degree=degree, salary=salary, experience=experience, user_job_details=user_job_details)
+    current_user_input = UserInput(
+        degree=degree, salary=salary, experience=experience, user_job_details=user_job_details)
+    write_json(current_user_input, 'data/user_input.json')
+    return current_user_input
